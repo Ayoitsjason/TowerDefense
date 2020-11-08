70be9aeefc3e70eb1a93dbfc01f1a89473539ad5
jason <jasonctle@gmail.com>
2020-11-08 11:08:08 -0800

    Finished code for first game background

--- towers/game.py
+++ towers/game.py
@@ -1,3 +1,48 @@
 import pygame
+import os
+import time
 
-pygame.init()
+
+class Game:
+    # initialize game
+    def __init__(self):
+        self.width = 1200
+        self.height = 700
+        self.win = pygame.display.set_mode((self.width, self.height))
+        self.enemies = []
+        self.towers = []
+        self.lives = 10
+        self.money = 100
+        self.bg = pygame.image.load(os.path.join(
+            "game_assets", "./support_towers/bg.png"))
+        self.bg = pygame.transform.scale(self.bg, (self.width, self.height))
+
+    # function to run game
+    def run(self):
+        run = True
+        clock = pygame.time.Clock()
+
+        while run:
+            clock.tick(60)
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    run = False
+
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    pass
+
+            self.draw()
+
+        pygame.quit()
+
+    def draw(self):
+        self.win.blit(self.bg, (0, 0))
+
+        for enemy in self.enemies:
+            enemy.draw(self.win)
+
+        pygame.display.update()
+
+
+g = Game()
+g.run()
